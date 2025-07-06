f68a5766dc01a1d1b8d225ea1103c86da422c57b
Amaiam Haque <haque.amaiam5@gmail.com>
2025-07-06 17:07:14 -0400

    tested interrupts

---
+++ IR_Test_Interrupt/IR_Test_Interrupt.ino
@@ -0,0 +1,111 @@
+#define DECODE_NEC
+#include <Arduino.h>
+#include <IRremote.hpp>        //includes the library needed for the IR remote
+const int IR_RECEIVE_PIN = 2;  //declares the incoming DATA pin
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("\nprogram start.");
+  //begins the receiver to receive incoming signals
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+  printActiveIRProtocols(&Serial);
+
+  pinMode(IR_RECEIVE_PIN, INPUT_PULLUP); //declares the pin as an interrupt
+  attachInterrupt(digitalPinToInterrupt(IR_RECEIVE_PIN), eee, CHANGE);
+  //declares the pin number, the method name, and activation state in order for interrupt to run
+  //there are three possible activation states
+  //RISING activates when there is a rise in voltage
+  //FALLING activates when there is a drop in voltage
+  //CHANGE activates when they is either a rise or drop in voltage
+
+}
+
+
+void loop() {
+  Serial.println("on standby...");
+  delay(2000);
+}
+
+
+void eee() {
+  if (IrReceiver.decode()) {
+    buttonPressed();
+    Serial.println();
+    IrReceiver.resume();
+  }
+}
+
+void buttonPressed() {
+  switch (IrReceiver.decodedIRData.command) {
+    case 69:
+      Serial.println("Button pressed: CH-");
+      break;
+    case 70:
+      Serial.println("Button pressed: CH");
+      break;
+    case 71:
+      Serial.println("Button pressed: CH+");
+      break;
+    case 68:
+      Serial.println("Button pressed: PREV");
+      break;
+    case 64:
+      Serial.println("Button pressed: NEXT");
+      break;
+    case 67:
+      Serial.println("Button pressed: PLAY/PAUSE");
+      break;
+    case 7:
+      Serial.println("Button pressed: VOL-");
+      break;
+    case 21:
+      Serial.println("Button pressed: VOL+");
+      break;
+    case 9:
+      Serial.println("Button pressed: EQ");
+      break;
+    case 22:
+      Serial.println("Button pressed: 0");
+      break;
+    case 25:
+      Serial.println("Button pressed: 100+");
+      break;
+    case 13:
+      Serial.println("Button pressed: 200+");
+      break;
+    case 12:
+      Serial.println("Button pressed: 1");
+      break;
+    case 24:
+      Serial.println("Button pressed: 2");
+      break;
+    case 94:
+      Serial.println("Button pressed: 3");
+      break;
+    case 8:
+      Serial.println("Button pressed: 4");
+      break;
+    case 28:
+      Serial.println("Button pressed: 5");
+      break;
+    case 90:
+      Serial.println("Button pressed: 6");
+      break;
+    case 66:
+      Serial.println("Button pressed: 7");
+      break;
+    case 82:
+      Serial.println("Button pressed: 8");
+      break;
+    case 74:
+      Serial.println("Button pressed: 9");
+      break;
+    default:
+      if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+        Serial.println("Received noise or an unknown (or not yet enabled) protocol");
+        IrReceiver.printIRResultShort(&Serial);
+      }
+      break;
+  }
+}
